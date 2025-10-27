14d9e19c0446b629cff2ffdf6ac9479355dff07b
Marcos <marcosvieira@tecgraf.puc-rio.br>
2025-10-27 12:35:27 -0300

    teste no tecgraf

--- T5/include/Table.h
+++ T5/include/Table.h
@@ -10,6 +10,9 @@ using TablePtr = std::shared_ptr<Table>;
 
 class Table : public Node
 {   
+    private:
+        glm::vec3 _pos;
+
 
     public :  
 
@@ -23,8 +26,19 @@ class Table : public Node
                 ~Leg();
         };
 
+        class Top : public Node
+        {
+            public: 
+                using TopPtr = std::shared_ptr<Top>;
+
+                static TopPtr Make(const glm::vec3& scale);
+                Top(const glm::vec3& scale);
+                ~Top();
+        };
+
+
         Table(const glm::vec3& pos);
-        void setupLegs();
+        void setup();
 
         ~Table();
 
--- T5/src/Table.cpp
+++ T5/src/Table.cpp
@@ -20,21 +20,33 @@ Table::Leg::Leg(const glm::vec3& relativePos, float height)
     trfCube->Scale(0.1f, height, 0.1f);
 
     AddShape(Cube::Make());
-    AddAppearance(Color::Make(0.8f, 0.5f, 0.3f));
+    SetTransform(trfCube);
+}
+
+Table::Top::TopPtr Table::Top::Make(const glm::vec3& scale)
+{
+    return std::make_shared<Top>(scale);
+}
+
+Table::Top::~Top() = default;
+
+Table::Top::Top(const glm::vec3& scale)
+{
+    auto trfCube = Transform::Make();
+    trfCube->Scale(scale.x, scale.y, scale.z);
+    AddShape(Cube::Make());
     SetTransform(trfCube);
 }
 
 
-
-Table::Table(const glm::vec3& pos)
+Table::Table(const glm::vec3& pos) : _pos(pos)
 {
-    auto trfCube = Transform::Make();
-    trfCube->Translate(pos.x, pos.y, pos.z);
-    trfCube->Scale(5.0f, 0.2f, 5.0f);
-
+    auto trfTable = Transform::Make();
+    trfTable->Translate(pos.x, pos.y, pos.z);
+    
     AddShape(Cube::Make());
     AddAppearance(Color::Make(1.0f, 0.3f, 0.3f));
-    SetTransform(trfCube);
+    SetTransform(trfTable);
 }
 
 TablePtr Table::Make(const glm::vec3& pos)
@@ -42,12 +54,13 @@ TablePtr Table::Make(const glm::vec3& pos)
     return std::make_shared<Table>(pos);
 }
 
-void Table::setupLegs()
+void Table::setup()
 {
-    AddNode(Table::Leg::Make(glm::vec3(0.4, -20, 0.4), 20));
-    AddNode(Table::Leg::Make(glm::vec3(-0.4, -20, 0.4), 20));
-    AddNode(Table::Leg::Make(glm::vec3(-0.4, -20, -0.4), 20));
-    AddNode(Table::Leg::Make(glm::vec3(0.4, -20, -0.4), 20));
+    AddNode(Table::Top::Make({5.0f, 0.2f, 5.0f}));
+    AddNode(Table::Leg::Make(glm::vec3(2.0, -20, 2.0), 20));
+    AddNode(Table::Leg::Make(glm::vec3(-2.0, -20, 2.0), 20));
+    AddNode(Table::Leg::Make(glm::vec3(-2.0, -20, -2.0), 20));
+    AddNode(Table::Leg::Make(glm::vec3(2.0, -20, -0.4), 20));
 }
 
 
