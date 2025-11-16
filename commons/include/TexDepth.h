9ccf58703ea4e4418d525ea18fc57fbdf7d78471
MarcosVieira71 <marcosvieira120404@gmail.com>
2025-11-16 02:31:10 -0300

    inclui novos objetos do grafo de cena

---
+++ commons/include/TexDepth.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <memory>
+
+class TexDepth;
+using TexDepthPtr = std::shared_ptr<TexDepth>; 
+
+
+#include "Appearance.h"
+#include <glm/glm.hpp>
+#include <string>
+
+class TexDepth : public Appearance {
+  unsigned int m_tex;
+  std::string m_varname;
+  int m_width;
+  int m_height;
+protected:
+  TexDepth (const std::string& varname, int width, int height);
+public:
+  static TexDepthPtr Make (const std::string& varname, int width, int height);
+  virtual ~TexDepth ();
+  unsigned int GetTexId () const;
+  void SetCompareMode ();
+  virtual void Load (StatePtr st);
+  virtual void Unload (StatePtr st);
+};
---
+++ commons/include/Texbuffer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+class TexBuffer;
+using TexBufferPtr = std::shared_ptr<TexBuffer>; 
+
+
+#include "Appearance.h"
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+class TexBuffer : public Appearance {
+  unsigned int m_tex;
+  unsigned int m_buffer;
+  std::string m_varname;
+protected:
+  TexBuffer (const std::string& varname, const std::vector<float>& data);
+public:
+  static TexBufferPtr Make (const std::string& varname, const std::vector<float>& data);
+  void SetData (const std::vector<float>& data);
+  std::vector<float> GetData () const;
+  virtual ~TexBuffer ();
+  unsigned int GetTexId () const;
+  const std::string& GetName () const;
+  virtual void Load (StatePtr st);
+  virtual void Unload (StatePtr st);
+};
